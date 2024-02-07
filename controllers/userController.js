500a834bc098d1aeb1bf6cb7382714123d53531b
adelorosia <adel_orosia@yahoo.com>
2024-02-07 14:02:30 +0100

    follow unfollow fertig

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,10 +14,14 @@ export const getAllUsers = asyncHandler(async (req, res) => {
 
 // REGISTER USER
 export const registerUser = asyncHandler(async (req, res) => {
-  const { firstName, lastName, email, password, gender } = req.body;
+  const { firstName, lastName, email, password, confirmPassword, gender } =
+    req.body;
   const userExist = await Users.findOne({ email });
   if (userExist)
     throw new Error("Oops! Looks like this email is already in our system");
+  if (password !== confirmPassword) {
+    throw new Error("Password and Confirm Password do not match.");
+  }
   try {
     await Users.create({
       firstName,
@@ -88,5 +92,151 @@ export const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+// Logout User
+export const logoutUser = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) throw new Error("No token provided. Unable to logout.");
+  const user = await Users.findOne({ refresh_token: token });
+  if (!user) throw new Error("User not found. Unable to logout.");
+  user.refresh_token = undefined;
+  await user.save();
+  res.clearCookie("token");
+  res.json("Logout successful.");
+});
+
 //CHANGE PASSWORD WENN USER IST LOGIN
-export const changePassword = asyncHandler(async (req, res) => {});
+export const changePassword = asyncHandler(async (req, res) => {
+  const userId = req.userId;
+  const { currentPassword, newPassword, confirmPassword } = req.body;
+  const user = await Users.findById(userId);
+  if (await user.isPasswordMatched(currentPassword)) {
+    if (newPassword !== confirmPassword) {
+      throw new Error("Password and Confirm Password do not match.");
+    }
+    user.password = newPassword;
+    await user.save();
+    res.json("Your password has been successfully changed.");
+  } else {
+    res.json("Please enter your password correctly");
+  }
+});
+
+//Block User
+export const blockUser = asyncHandler(async (req, res) => {
+  const loginUserId = req.userId;
+  const targetUserId = req.body.targetUserId;
+  const loginUser = await Users.findById(loginUserId);
+  const targetUser = await Users.findById(targetUserId);
+  if (loginUser.isAdmin) {
+    await Users.findByIdAndUpdate(targetUserId, { isBlocked: true });
+    res.json(
+      `${
+        targetUser.firstName + " " + targetUser.lastName
+      } has been successfully blocked`
+    );
+  } else {
+    res.json("You are not allowed to block this user.");
+  }
+});
+
+//unBlock User
+export const unBlockUser = asyncHandler(async (req, res) => {
+  const loginUserId = req.userId;
+  const targetUserId = req.body.targetUserId;
+  const loginUser = await Users.findById(loginUserId);
+  const targetUser = await Users.findById(targetUserId);
+  if (loginUser.isAdmin) {
+    await Users.findByIdAndUpdate(targetUserId, { isBlocked: false });
+    res.json(
+      `${
+        targetUser.firstName + " " + targetUser.lastName
+      } has been successfully unblocked`
+    );
+  } else {
+    res.json("You are not allowed to unblock this user.");
+  }
+});
+
+// Delete Account
+export const deleteAccount = asyncHandler(async (req, res) => {
+  const loginUserId = req.userId;
+  const targetUserId = req.body.targetUserId;
+  const loginUser = await Users.findById(loginUserId);
+  if (loginUser.isAdmin || loginUserId === targetUserId) {
+    await Users.findByIdAndDelete(targetUserId);
+    res.json("Account successfully deleted.");
+  } else {
+    res.json("You do not have permission to delete this account.");
+  }
+});
+
+// Edit User Info
+export const editProfileInfo = asyncHandler(async (req, res) => {
+  try {
+    const { firstName, lastName, gender, bio } = req.body;
+    const loginUserId = req.userId;
+    await Users.findByIdAndUpdate(loginUserId, {
+      firstName,
+      lastName,
+      gender,
+      bio,
+    });
+    res.json({ message: "Profile information updated successfully." });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// Follow User
+export const FollowUser = asyncHandler(async (req, res) => {
+  const loginUserId = req.userId;
+  const targetUserId = req.body.targetUserId;
+  const targetUser = await Users.findById(targetUserId);
+  const followers = targetUser.followers.find(
+    (user) => user.toString() === loginUserId.toString()
+  );
+  if (followers) throw new Error("You are already following this user.");
+  await Users.findByIdAndUpdate(
+    targetUserId,
+    {
+      $push: { followers: loginUserId },
+      isFollowing: true,
+    },
+    { new: true }
+  );
+  await Users.findByIdAndUpdate(
+    loginUserId,
+    {
+      $push: { following: targetUserId },
+    },
+    { new: true }
+  );
+  res.json("You are now following the user.");
+});
+
+// UnFollow User
+export const UnFollowUser = asyncHandler(async (req, res) => {
+  const loginUserId = req.userId;
+  const targetUserId = req.body.targetUserId;
+  const targetUser = await Users.findById(targetUserId);
+  const followers = targetUser.followers.find(
+    (user) => user.toString() === loginUserId.toString()
+  );
+  if (!followers) throw new Error("You are not following this user.");
+  await Users.findByIdAndUpdate(
+    targetUserId,
+    {
+      $pull: { followers: loginUserId },
+      isFollowing: false,
+    },
+    { new: true }
+  );
+  await Users.findByIdAndUpdate(
+    loginUserId,
+    {
+      $pull: { following: targetUserId },
+    },
+    { new: true }
+  );
+  res.json("You have unfollowed the user.");
+});
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,47 @@
 import express from "express";
-import { getAllUsers, loginUser, registerUser } from "../controllers/userController.js";
+import {
+  FollowUser,
+    UnFollowUser,
+    blockUser,
+  changePassword,
+  deleteAccount,
+  editProfileInfo,
+  getAllUsers,
+  loginUser,
+  logoutUser,
+  registerUser,
+  unBlockUser,
+} from "../controllers/userController.js";
 import { refreshToken } from "../controllers/refreshToken.js";
+import { verifyToken } from "../middlewares/tokens/verifyToken.js";
 
 const router = express.Router();
 
-router.get("/api/token",refreshToken)
-router.get("/api/users",getAllUsers)
+// Token
+router.get("/api/token", refreshToken);
+
+// Get All Users
+router.get("/api/users", getAllUsers);
+
+//Register And Login And Logout
 router.post("/api/users/register", registerUser);
 router.post("/api/users/login", loginUser);
+router.delete("/api/users/logout", logoutUser);
+
+// Follow And unFollow
+router.post("/api/users/follow",verifyToken,FollowUser);
+router.post("/api/users/unfollow",verifyToken,UnFollowUser);
+
+
+// Block and Unblock User
+router.put("/api/users/block",verifyToken,blockUser);
+router.put("/api/users/unblock",verifyToken,unBlockUser);
+
+// Change Password and Photo
+router.put("/api/users/change_password",verifyToken,changePassword);
+router.put("/api/users/edit_profile_info",verifyToken,editProfileInfo);
+
+//Delete Account by Admin and User
+router.delete("/api/users/delete_account",verifyToken,deleteAccount)
 
 export default router;
